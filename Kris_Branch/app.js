f8fd5cbbcf44deb4127a841b32717530bdcef9bd
kriswasemiller <70985555+kriswasemiller@users.noreply.github.com>
2021-02-06 13:04:18 -0600

    update branch

--- Kris_Branch/app.js
+++ Kris_Branch/app.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 function makeMap() {
     var queryUrl = "https://covid.ourworldindata.org/data/owid-covid-data.json"
 
+
     // Perform a GET request to the query URL
     $.ajax({
         type: "GET",
@@ -19,5 +20,12 @@ function makeMap() {
             alert("Status: " + textStatus);
             alert("Error: " + errorThrown);
         }
+    });
+
+    d3.csv("Resources/covid19_vaccinations_in_the_united_states.csv").then(function(data2) {
+        console.log(data2);
+
+        // buildPlot(data2);
+
     });
 }
